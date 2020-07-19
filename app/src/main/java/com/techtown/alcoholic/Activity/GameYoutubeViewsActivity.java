815d4783e1c6566f061ab7dedcc556f0d818e35b
sang <sang_hk@naver.com>
2020-07-20 02:06:56 +0900

    유튜브조회수 게임 추가

---
+++ app/src/main/java/com/techtown/alcoholic/Activity/GameYoutubeViewsActivity.java
@@ -0,0 +1,16 @@
+package com.techtown.alcoholic.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.techtown.alcoholic.R;
+
+public class GameYoutubeViewsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_youtube_views);
+    }
+}
