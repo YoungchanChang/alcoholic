84d81c71ce796b2b6a861a69b33727250b919ad4
San <san0558@naver.com>
2020-07-20 08:59:55 +0900

    New Room2

--- app/src/main/java/com/techtown/alcoholic/Activity/RoomStart2Activity.java
+++ app/src/main/java/com/techtown/alcoholic/Activity/RoomStart2Activity.java
@@ -3,14 +3,20 @@ package com.techtown.alcoholic.Activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
 import com.techtown.alcoholic.R;
 
 public class RoomStart2Activity extends AppCompatActivity {
 
+    ImageView image_gif;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_room_start2);
+
+        image_gif = findViewById(R.id.image_gif);
+
     }
 }
