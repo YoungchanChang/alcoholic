f2617df7ff6f4fd0802779ea2d9978d84e44f2e9
sang <sang_hk@naver.com>
2020-07-19 23:17:05 +0900

    초성게임 액티비티 추가

---
+++ app/src/main/java/com/techtown/alcoholic/Activity/GameInitialSound.java
@@ -0,0 +1,16 @@
+package com.techtown.alcoholic.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.techtown.alcoholic.R;
+
+public class GameInitialSound extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_initial_sound);
+    }
+}
