3c21b22c1945c9b38905c15f40b5cc9940618484
San <san0558@naver.com>
2020-07-19 19:30:13 +0900

    git last test

--- app/src/main/java/com/techtown/alcoholic/Activity/RoomActivity.java
+++ app/src/main/java/com/techtown/alcoholic/Activity/RoomActivity.java
@@ -7,10 +7,18 @@ import androidx.fragment.app.FragmentTransaction;
 
 
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.EncodeHintType;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.common.BitMatrix;
+import com.journeyapps.barcodescanner.BarcodeEncoder;
 import com.techtown.alcoholic.Fragment.RoomGameListFragment;
 import com.techtown.alcoholic.Fragment.RoomInfoFragment;
 
@@ -29,8 +37,10 @@ public class RoomActivity extends AppCompatActivity {
     RoomGameListFragment roomGameListFragment;
     RoomInfoFragment roomInfoFragment;
     Context context;
-    Context context2;
-
+    //QR코드가 보여질 이미지 부분
+    private ImageView imageViewQRCode;
+    //QR코드 값 받기
+    private String textForQRCode;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -70,6 +80,28 @@ public class RoomActivity extends AppCompatActivity {
     }
 
 
+    public void QRFunction(){
+
+        imageViewQRCode = (ImageView)findViewById(R.id.qrCode);
+
+
+        textForQRCode = "테스트";
+
+        Log.d(TAG, "onCreate: ");
+        MultiFormatWriter multiFormatWriter = new MultiFormatWriter();
+        try{
+            /* Encode to utf-8 */
+            Hashtable hints = new Hashtable();
+            hints.put(EncodeHintType.CHARACTER_SET, "utf-8");
+
+            //width랑 height에서 QR코드 이미지 크기 조정
+            BitMatrix bitMatrix = multiFormatWriter.encode(textForQRCode, BarcodeFormat.QR_CODE,200,200, hints);
+            BarcodeEncoder barcodeEncoder = new BarcodeEncoder();
+            Bitmap bitmap = barcodeEncoder.createBitmap(bitMatrix);
+            imageViewQRCode.setImageBitmap(bitmap);
+
+        }catch (Exception e){}
+    }
 
     public void setFrag(int n) {
         fragmentManager = getSupportFragmentManager();
