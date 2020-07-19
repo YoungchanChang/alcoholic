3b2e4fcc2c637a44282c85b96502d4cf8669df04
yuvin <dbqls9713@naver.com>
2020-07-19 22:12:38 +0900

    yuvin commit 메니페스트 시작 액티비티 변경, 메인 액티비티 쉐킷 버튼 추가

--- app/src/main/java/com/techtown/alcoholic/Activity/RoomStartActivity.java
+++ app/src/main/java/com/techtown/alcoholic/Activity/RoomStartActivity.java
@@ -2,19 +2,27 @@ package com.techtown.alcoholic.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.pedro.library.AutoPermissions;
 import com.pedro.library.AutoPermissionsListener;
 import com.techtown.alcoholic.R;
 
-public class RoomStartActivity extends AppCompatActivity implements AutoPermissionsListener {
+public class RoomStartActivity extends AppCompatActivity implements AutoPermissionsListener, View.OnClickListener {
+
+    Button btnShakeIt;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_room_start);
+        btnShakeIt = findViewById(R.id.btnShakeIt);
+        btnShakeIt.setOnClickListener(this);
 
         AutoPermissions.Companion.loadAllPermissions(this, 101);
     }
@@ -36,4 +44,14 @@ public void onDenied(int requestCode, String[] permissions) {
 public void onGranted(int requestCode, String[] permissions) {
         Toast.makeText(this, "permissions granted : " + permissions.length, Toast.LENGTH_LONG).show();
         }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btnShakeIt:
+                Intent intent = new Intent(RoomStartActivity.this,GameShakeItActivity.class);
+                startActivity(intent);
+                break;
+        }
+    }
 }
