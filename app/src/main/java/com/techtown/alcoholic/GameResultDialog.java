9060d7af454d6a8c5d76b8f9c9d6854f1071e3d3
sang <sang_hk@naver.com>
2020-07-20 12:35:45 +0900

    버전업 후 레이아웃 안되는 태그 제거

--- app/src/main/java/com/techtown/alcoholic/GameResultDialog.java
+++ app/src/main/java/com/techtown/alcoholic/GameResultDialog.java
@@ -10,5 +10,7 @@ public class GameResultDialog extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_result_dialog);
+
+
     }
 }
