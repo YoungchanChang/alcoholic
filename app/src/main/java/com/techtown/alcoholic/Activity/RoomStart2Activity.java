369a80a4c3590b135b76fa82d94db722ddcbd4e2
San <san0558@naver.com>
2020-07-20 11:18:20 +0900

    New Room2

--- app/src/main/java/com/techtown/alcoholic/Activity/RoomStart2Activity.java
+++ app/src/main/java/com/techtown/alcoholic/Activity/RoomStart2Activity.java
@@ -1,16 +1,28 @@
 package com.techtown.alcoholic.Activity;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 
 import com.techtown.alcoholic.R;
 import com.bumptech.glide.Glide;
-public class RoomStart2Activity extends AppCompatActivity {
+public class RoomStart2Activity extends AppCompatActivity implements View.OnClickListener{
+    private static final String TAG = "RoomStart";
 
     ImageView image_gif;
+    ImageButton imageMakeRoom;
 
+    AlertDialog.Builder ad;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +32,69 @@ public class RoomStart2Activity extends AppCompatActivity {
         image_gif = findViewById(R.id.image_gif);
         Glide.with(this).asGif().load(R.drawable.tenor).into(image_gif);
 
+        imageMakeRoom = findViewById(R.id.imageMakeRoom);
+        imageMakeRoom.setOnClickListener(this);
 
+
+
+
+
+
+
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.imageMakeRoom:
+                showDialogue();
+                ad.show();
+
+                break;
+        }
+    }
+
+    public void showDialogue(){
+        ad = new AlertDialog.Builder(RoomStart2Activity.this);
+        ad.setTitle("방장 닉네임 설정");       // 제목 설정
+        ad.setMessage("방장의 닉네임을 설정해 주세요.");   // 내용 설정
+
+        final EditText et = new EditText(RoomStart2Activity.this);
+        ad.setView(et);
+
+
+        ad.setPositiveButton("취소", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+
+                dialog.dismiss();
+
+            }
+        });
+
+        ad.setNegativeButton("확인", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+                String value = et.getText().toString();
+
+                //로그인 성공시에 유저 info 저장시에 쓰일 SharedPref
+                SharedPreferences pref = getSharedPreferences("USER_INFO", Activity.MODE_PRIVATE);;
+                SharedPreferences.Editor editor = pref.edit();
+                editor.putString("captainName", value);
+                editor.commit();
+
+                dialog.dismiss();     //닫기
+
+                Intent goHome = new Intent(getApplicationContext(), RoomActivity.class);
+                //user_id를 전달하면 메인홈에서 바로 SELECT문으로 회원정보 가져올 것이다.
+                startActivity(goHome);
+                finish();
+
+
+            }
+        });
     }
 }
